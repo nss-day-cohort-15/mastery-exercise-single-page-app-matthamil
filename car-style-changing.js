96d4ec2f94639fb8435b6f19ba660d781ce403fc
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-29 23:44:41 -0500

    Submission 1 for SPA Quiz

---
+++ car-style-changing.js
@@ -0,0 +1,35 @@
+var CarLot = (function carStylesModule(carLot = {}) {
+  carLot.resetBorders = () => {
+    let cars = document.getElementsByClassName('car');
+    let carObjects = carLot.getInventory();
+
+    for (let i = 0; i < cars.length; i++) {
+      cars[i].style.border = `3px solid ${getCarBorderColor(i)}`;
+      cars[i].style.backgroundColor = '#fff';
+    }
+
+    function getCarBorderColor(id) {
+      return carObjects[id].color;
+    }
+  };
+
+  carLot.selectCar = (carElement, color) => {
+    // Traverse to the wrapping div on the clicked element
+    try {
+      var node = carElement.parentNode;
+      while (node != null) {
+        if (node.classList.contains('car')) {
+          node.style.border = '6px solid';
+          node.style.backgroundColor = color;
+        }
+        node = node.parentNode;
+      }
+    } catch (e) {
+      // Prevent the error from stopping all code execution
+      // Logs error instead
+      console.log(e);
+    }
+  };
+
+  return carLot;
+})(CarLot);
--- eventHandlers.js
+++ eventHandlers.js
@@ -7,19 +7,16 @@ var CarLot = (function eventHandlersModule(carLot = {}) {
 
     // DOM Element containing the car
     var carElement = document.createElement('div');
-    carElement.classList ="col-sm-4 car";
-    carElement.id = `car-${index}`;
-
-    console.dir(carElement);
+    carElement.classList ="col-sm-4";
 
     var carContent = `
-      <div class="panel panel-default" style="border:3px solid ${car.color}">
+      <div class="panel panel-default car" id="car-${index}" style="border:3px solid ${car.color}">
         <div class="panel-heading">
          <h1>${car.make} ${car.model}, ${car.year}<h1>
         </div>
         <div class="panel-body">
          <h2>$${car.price}</h2>
-         <section>${car.description}</section>
+         <section id="car-${index}-desc">${car.description}</section>
          <section>Color: ${car.color}</section>
           <section>
            <h2>
@@ -41,11 +38,86 @@ var CarLot = (function eventHandlersModule(carLot = {}) {
 
   carLot.activateEvents = () => {
     inventory = carLot.getInventory();
-    console.log(inventory);
     inventory.forEach(loadCarIntoDom);
+    setEventHandlers();
   };
 
   carLot.loadInventory(carLot.activateEvents);
+
+  function setEventHandlers() {
+    var carElements = document.getElementsByClassName('car');
+    var input = document.querySelector('input');
+
+    // Adds a new property to each car to be used when determining which car description to edit
+    inventory = inventory.map(car => {
+      car.editMode = false;
+      return car;
+    });
+
+    // Resets borders and background colors on all cars
+    function resetCars() {
+      carLot.resetBorders();
+      inventory.forEach(car => car.editMode = false);
+    }
+
+    for (let i = 0; i < carElements.length; i++) {
+      // Add click event where borders get larger and background color changes
+      carElements[i].addEventListener('click', e => {
+        resetCars();
+        inventory[i].editMode = true;
+        carLot.selectCar(e.target, '#FFA000');
+        input.value = '';
+        input.focus();
+
+        // Event listener for input field to update car description
+        input.addEventListener('keyup', e => {
+          if (inventory[i].editMode) {
+            inventory[i].description = input.value;
+
+            // Targeting section with description
+            document.getElementById(`car-${i}-desc`).innerHTML = inventory[i].description;
+          }
+        });
+      });
+    }
+
+    input.addEventListener('keyup', e => {
+      if (e.keyCode == 13 && isEditModeOn()) {
+        // Reset the input field when enter is pressed
+        input.value = '';
+        resetCars();
+      }
+    });
+
+    function isEditModeOn() {
+       var isEditModeSelected = inventory.reduce((editStatus, car) => {
+        if (car.editMode === true) {
+          editStatus = true;
+        }
+        return editStatus;
+      }, false);
+
+      if (!isEditModeSelected) {
+        alert('Please select a car.');
+      }
+
+      return isEditModeSelected;
+    }
+
+    document.querySelector('button').addEventListener('click', e => {
+      if (isEditModeOn()) {
+        // Determine which car is in editMode
+        for (let i = 0; i < inventory.length; i++) {
+          if (inventory.editMode) {
+            document.getElementById(`car-${i}-desc`).innerHTML = inventory[i].description;
+          }
+        }
+      }
+
+      input.value = '';
+      resetCars();
+    });
+  }
 
   return carLot;
 })(CarLot);
